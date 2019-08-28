e058603891559553552b7ea2a19946ee579504fd
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-08-28 17:06:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleSearchValueInBoolArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleSearchValueInBoolArray {
+    class Program {
+        static void Main(string[] args) {
+            Check();
+            Console.ReadKey();
+        }
+        public static bool Check() {
+            object[] a = { 1, 2, 3 };
+            object x = 2;
+            if (a.Contains(x)) { Console.WriteLine("true"); return true; }
+            return false;
+        }
+    }
+}
